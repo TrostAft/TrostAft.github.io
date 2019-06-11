c2e66994769e6dc965464d5a317a0c74ccc12f4f
abhijit-c <abhijit9331@gmail.com>
2019-06-11 04:08:18 -0400

    Finally finished cosine interpolation, works fine.

--- TeXsrc/SpectralDifferentiation/code/cos_interp.cpp
+++ TeXsrc/SpectralDifferentiation/code/cos_interp.cpp
@@ -23,22 +23,32 @@ int main(int argc, char **argv)
   }
   int N = atoi(argv[1]), M = atoi(argv[2]);
 
+  printf("# Interpolated cosine on [0, 2pi] to %d grid points\n", M);
+  printf("# num_pts\t||phi(x)-cos(x)||_\\infty\n");
+
   fftw_complex *fxx, *out;
   out = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * M);
-  fxx = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * N);
-
-  for (int k = 0; k < N; k++)
-  { //Build [0,2*pi] and map cos() onto it.
-    fxx[k][0] = cos(k*2*M_PI / N); fxx[k][1] = 0;
-  }
-  interpft(fxx, N, out, M);
-
-  printf("(%f, %f)", out[0][0], out[0][1]);
-  for (int k = 1; k < M; k++)
+  while (2*N < M)
   {
-    printf(",(%f, %f)", out[k][0], out[k][1]);
-  }
-  printf("\n");
+    fxx = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * N);
 
-  fftw_free(fxx); fftw_free(out);
+    for (int k = 0; k < N; k++)
+    { //Build [0,2*pi] and map cos() onto it.
+      fxx[k][0] = cos(k*2*M_PI / N); fxx[k][1] = 0;
+    }
+    for (int k = 0; k < M; k++) { out[k][0] = 0; out[k][1] = 0; }
+    interpft(fxx, N, out, M);
+
+    double err = 0.0;
+    for (int k = 0; k < M; k++)
+    {
+      err = fmax(err, fabs( out[k][0] - cos(k*2*M_PI/M) ));
+    }
+    printf("%d\t%e\n", N, err);
+    N = N + 1;
+
+    fftw_free(fxx); 
+  }
+
+  fftw_free(out);
 }
